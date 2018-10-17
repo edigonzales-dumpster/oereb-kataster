41aed7898d5dec83bdf094c08243a207069d5b12
Stefan Ziegler <edi.gonzales@gmail.com>
2018-10-17 20:29:08 +0200

    start with proof of concept web service

---
+++ web-service/src/main/java/ch/so/agi/oereb/webservice/controllers/GetEGRIDController.java
@@ -0,0 +1,13 @@
+package ch.so.agi.oereb.webservice.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//200: OK, Antwort konnte erstellt werden
+//204: Kein Grundstück gefunden 
+//500: Andere Fehler 
+
+public class GetEGRIDController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+}
