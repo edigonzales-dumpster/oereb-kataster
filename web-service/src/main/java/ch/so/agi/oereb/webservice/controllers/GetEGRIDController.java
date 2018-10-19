5cb28e7913e2bdc1919d64de329875dcf838a383
Stefan Ziegler <edi.gonzales@gmail.com>
2018-10-19 22:23:50 +0200

    wip

--- web-service/src/main/java/ch/so/agi/oereb/webservice/controllers/GetEGRIDController.java
+++ web-service/src/main/java/ch/so/agi/oereb/webservice/controllers/GetEGRIDController.java
@@ -6,13 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.admin.geo.schemas.v_d.oereb._1_0.extract.GetEGRIDResponseType;
@@ -38,15 +36,11 @@ public class GetEGRIDController {
                 
         double[] coords = validateCoordinateRequestParam(xy);
         GetEGRIDResponseType getEGRIDResponseType = getEGRIDResponseTypeService.getGetEGRIDResponseTypeByXY(coords[0], coords[1]);
-        log.info(getEGRIDResponseType.getEgridAndNumberAndIdentDN().toString());
-        
-        /*
-        GetEGRIDResponseType getEGRIDResponseType = egridEntityService.getGetEGRIDResponseTypeByXY(coords[0], coords[1]);
         
         if (getEGRIDResponseType.getEgridAndNumberAndIdentDN().size() == 0) {
             log.warn("No egrid found at: " + xy);
             return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-        }*/
+        }
         return ResponseEntity.ok(getEGRIDResponseType);
         
     }
